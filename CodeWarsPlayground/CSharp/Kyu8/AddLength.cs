27b114cc03274463182dc67422a65db970849c15
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-01-07 21:45:33 +0100

    AddLength

---
+++ CodeWarsPlayground/CSharp/Kyu8/AddLength.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu8
+{
+    public class AddLengthClass
+    {
+
+        public static string[] AddLength(string str)
+        {
+            var x = str.Split(" ");
+            for (int i = 0; i < x.Length; i++)
+            {
+                x[i] = x[i] + " " + x[i].Count().ToString();
+            }
+            return x;
+        }
+
+
+    }
+}
