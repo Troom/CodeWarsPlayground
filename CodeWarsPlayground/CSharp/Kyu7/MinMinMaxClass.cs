6e13a8e20631b22b925c56b8469082cb5fc01719
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-01-08 20:06:14 +0100

    MinMinMax BeginnerReduceButGrpw

---
+++ CodeWarsPlayground/CSharp/Kyu7/MinMinMaxClass.cs
@@ -0,0 +1,33 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu7
+{
+    public class MinMinMaxClass
+    {
+        public static int[] MinMinMax(int[] array)
+        {
+            var result = new List<int>
+            {
+                array.Min()
+            };
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (!array.Contains(result.FirstOrDefault() + i))
+                {
+                    result.Add(result.FirstOrDefault() + i);
+                    break;
+                }
+            }
+            result.Add(array.Max());
+            return result.ToArray();
+        }
+        public static int[] MinMinMax1(int[] array)
+        {
+            int smallest = array.Min();
+            int largest = array.Max();
+            for (int i = smallest + 1; i < largest; i++)
+                if (!array.Contains(i)) return new int[] { smallest, i, largest };
+            return new int[] { };
+        }
+        public static int[] MinMinMax2(int[] a) => new[] { a.Min(), Enumerable.Range(a.Min(), a.Max()).Except(a).First(), a.Max() };
+
+    }
+}
---
+++ CodeWarsPlayground/CSharp/Kyu8/BeginnerReduceButGrow.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu8
+{
+    internal class BeginnerReduceButGrow
+    {
+        public static int Grow(int[] x)
+        {
+            int result = 1;
+            for (int i = 0; i < x.Length; i++)
+            {
+                result = result * x[i];
+            }
+            return result;
+        }
+
+        public static int GrowAggregate(int[] x) //Worst performace
+        {
+            return x.Aggregate((a, b) => a * b);
+        }
+    }
+}
