ba5e0eae81d28f7c429ab624e79d68c61e620962
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-01-10 13:34:26 +0100

    Added MovingZerosToTheEnd

---
+++ CodeWarsPlayground/CSharp/Kyu5/MovingZerosToTheEnd.cs
@@ -0,0 +1,29 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu5
+{
+    public static  class MovingZerosToTheEnd
+    {
+        public static int[] MoveZeroes(int[] arr)
+        {
+            var result = new List<int>();
+            int counter = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                {
+                    counter++;
+                }
+                else
+                {
+                    result.Add(arr[i]);
+                }
+            }
+            for (int i = 0; i < counter; i++)
+            {
+                result.Add(0);
+            }
+            return result.ToArray();
+        }
+
+        public static int[] MoveZeroesOrderBy(int[] arr) => arr.OrderBy(x => x == 0).ToArray();
+    }
+}
