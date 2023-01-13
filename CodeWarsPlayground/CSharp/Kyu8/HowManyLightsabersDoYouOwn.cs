b9915e95aba3abfc0619bf17569e8e300b02790f
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-01-13 21:30:01 +0100

    Added HowManyLightsabersDoYouOwn

---
+++ CodeWarsPlayground/CSharp/Kyu8/HowManyLightsabersDoYouOwn.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu8
+{
+    internal class HowManyLightsabersDoYouOwnClass
+    {
+        public static int HowManyLightsabersDoYouOwn(string name) => name == "Zach" ? 18 : 0;
+    }
+}
