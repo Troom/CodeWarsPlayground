5f83173b919993fe572d5a6c967f1ed0ceea96e1
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-01-14 19:22:16 +0100

    Added FindTheUniqueNumber

---
+++ CodeWarsPlayground/CSharp/Kyu6/FindTheUniqueNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsPlayground.CSharp.Kyu6
+{
+    internal static class FindTheUniqueNumber
+    {
+        public static int GetUniqueLinq(IEnumerable<int> numbers) => numbers.GroupBy(x=>x).Where(y=>y.Count() == 1).Select(z=>z.Key).FirstOrDefault();
+
+    }
+}
