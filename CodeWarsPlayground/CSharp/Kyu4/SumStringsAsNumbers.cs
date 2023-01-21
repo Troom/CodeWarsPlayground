b2a9e554a0bec707700a6489d9dd9c2df06e44d4
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-01-21 19:32:22 +0100

    Added SumStringsAsNumbers

---
+++ CodeWarsPlayground/CSharp/Kyu4/SumStringsAsNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+
+namespace CodeWarsPlayground.CSharp.Kyu4
+{
+    internal static class SumStringsAsNumbers
+    {
+
+        public static string sumStrings(string a, string b)
+        {
+            if (string.IsNullOrEmpty(a))
+                a = "0";
+            if (string.IsNullOrEmpty(b))
+                b = "0";
+            return (BigInteger.Parse(a) + BigInteger.Parse(b)).ToString();
+        }
+
+        public static string sumStringsTryParse(string a, string b)
+        {
+            BigInteger.TryParse(a, out BigInteger bigA);
+            BigInteger.TryParse(b, out BigInteger bigB);
+
+            return (bigA + bigB).ToString();
+        }
+
+    }
+}
