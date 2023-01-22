c5f048241ed1ea651e9d87148bc6d5de997f43fc
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-01-22 20:47:26 +0100

    Added You'reASquare!

---
+++ CodeWarsPlayground/CSharp/Kyu7/You'reASquare!.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu7
+{
+    internal static class You_reASquare_
+    {
+        public static bool IsSquare(int n) => Int32.TryParse(Math.Sqrt(n).ToString(), out int result);
+    }
+}
