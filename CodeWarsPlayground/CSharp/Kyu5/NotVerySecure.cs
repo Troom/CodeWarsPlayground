170bcc5b21298e1118949dc55a63be421440c291
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-01-25 21:26:14 +0100

    Added NotVerySecure

---
+++ CodeWarsPlayground/CSharp/Kyu5/NotVerySecure.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeWarsPlayground.CSharp.Kyu5
+{
+    internal static class NotVerySecure
+    {
+        public static bool Alphanumeric(string str) => !string.IsNullOrEmpty(str) ? new Regex("^[a-zA-Z0-9]*$").IsMatch(str) : false;
+        public static bool AlphanumericWithoutRegex(string str)
+        {
+            return str.All(c => Char.IsLetterOrDigit(c)) && !string.IsNullOrEmpty(str);
+        }
+    }
+}
