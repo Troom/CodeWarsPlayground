68804bbc2dc5d3b7ad834fb849374852becad41e
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-01-27 23:00:29 +0100

    Added Remove_the_parentheses

---
+++ CodeWarsPlayground/CSharp/Kyu6/Remove the parentheses.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeWarsPlayground.CSharp.Kyu6
+{
+    internal static class Remove_the_parentheses
+    {
+            public static string RemoveParentheses(string s)
+            {
+                do
+                    s = Regex.Replace(s, @"\([^()]*\)", string.Empty);
+                while 
+                    (s.Contains("(") || s.Contains("}"));
+                return s;
+            }
+
+            public static string RemoveParenthesesRecursion(string s)
+                             => s.Contains('(') ? RemoveParenthesesRecursion(Regex.Replace(s, @"\([^()]*\)", "")) : s;
+
+    }
+}
