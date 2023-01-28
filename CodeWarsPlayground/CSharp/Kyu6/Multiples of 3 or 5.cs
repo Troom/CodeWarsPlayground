85a9b587c9c461a5a55888be9160b19939b3056c
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-01-28 21:07:59 +0100

    Added Multiples_of_3_or_5

---
+++ CodeWarsPlayground/CSharp/Kyu6/Multiples of 3 or 5.cs
@@ -0,0 +1,33 @@
+﻿using System.Dynamic;
+
+namespace CodeWarsPlayground.CSharp.Kyu6
+{
+    internal static class Multiples_of_3_or_5
+    {
+        public static int Solution(int value)
+        {
+            value = 20;
+            int result = 0;
+            do
+            {
+                value--;
+                if (value % 5 == 0)
+                { 
+                    result += value;
+                    continue;
+                }
+
+                if (value % 3 == 0)
+                    result += value;
+            } while (value > 2);
+
+            return result;
+        }
+
+
+        public static int SolutionRange(int n)
+        {
+            return Enumerable.Range(0, n).Where(e => e % 3 == 0 || e % 5 == 0).Sum();
+        }
+    }
+}
