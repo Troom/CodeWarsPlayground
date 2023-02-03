920cb4e3ab0f38e6176b597c411d15b74023a871
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-02-03 19:29:49 +0100

    public class Grasshopper___Terminal_Game_1

---
+++ CodeWarsPlayground/CSharp/Kyu8/Grasshopper - Terminal Game 1.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu8
+{
+    internal class Grasshopper___Terminal_Game_1
+    {
+        public class Hero
+        {
+            public string Name { get; set; }
+            public string Position { get; set; } = "00";
+            public float Health { get; set; } = 100;
+            public float Damage { get; set; } = 5;
+            public int Experience { get; set; } = 0;
+
+            public Hero(string name = "Hero")
+            {
+                this.Name = name;
+            }
+        }
+    }
+}
