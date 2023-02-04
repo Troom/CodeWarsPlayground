777805627dad21bb6f619099404f7fe534f8a2a9
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-02-04 22:40:16 +0100

    no message

---
+++ CodeWarsPlayground/CSharp/Kyu8/Convert a Boolean to a String.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu8
+{
+    internal class Convert_a_Boolean_to_a_String
+    {
+        public static string BooleanToString(bool b) => b ? "True" : "False";
+    }
+}
