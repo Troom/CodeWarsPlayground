6df2a0fbbb817f4e25e4b44e7846765b3a9bb811
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-02-05 12:23:29 +0100

    Added Price_of_Mangoes

---
+++ CodeWarsPlayground/CSharp/Kyu8/Price of Mangoes.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu8
+{
+    internal static class Price_of_Mangoes
+    {
+        public static int Mango(int quantity, int price) => quantity / 3 > 0 ? (quantity - (quantity / 3)) * price : quantity * price;
+    }
+}
