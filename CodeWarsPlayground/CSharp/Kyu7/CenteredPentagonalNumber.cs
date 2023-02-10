dc2c1a98b67b7cdbf5041acd3cca12249edb40a4
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-02-10 15:09:52 +0100

    Added CenteredPentagonalNumber

---
+++ CodeWarsPlayground/CSharp/Kyu7/CenteredPentagonalNumber.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu7
+{
+    internal static class CenteredPentagonalNumber
+    {
+        //Math.Pow oparates on floating-point types, which are by definition inaccurate.
+        public static long Pentagonal(long n) => n > 0 ? (5 * n * n - 5 * n + 2) / 2 : -1;
+    }
+}
