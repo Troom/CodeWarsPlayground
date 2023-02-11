ccca813fb961a59ca90ea56b069c3e6372a5fd00
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-02-11 21:13:10 +0100

    Added OverTheRoad

---
+++ CodeWarsPlayground/CSharp/Kyu7/OverTheRoad.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu7
+{
+    internal class OverTheRoadClass
+    {
+        public static long OverTheRoad(long address, long n) => (n * 2) + 1 - address;
+    }
+}
