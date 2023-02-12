a772a026e80e007243ad41fef92c58a8dd66a51a
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-02-12 20:10:48 +0100

    Added StringAddiitionNeedHighSpeed. StringBuilder is great for large numbers of concatenations

---
+++ CodeWarsPlayground/CSharp/Kyu7/StringAddiitionNeedHighSpeed.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace CodeWarsPlayground.CSharp.Kyu7
+{
+    internal static class StringAddiitionNeedHighSpeed
+    {
+        public static string Performance(Func<int> method)
+        {
+            //StringBuilder is great for large numbers of concatenations
+            StringBuilder sb = new StringBuilder(); 
+
+            for (int i = 0; i < 150000; i++)
+            {
+                sb.Append(method.Invoke());
+            }
+            return sb.ToString();
+        }
+    }
+}
