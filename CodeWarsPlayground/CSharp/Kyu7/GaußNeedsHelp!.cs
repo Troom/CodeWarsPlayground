d40e501af2014fc77f10e380a0af8f294549b736
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-02-13 14:28:40 +0100

    Added GaußNeedsHelp

---
+++ CodeWarsPlayground/CSharp/Kyu7/GaußNeedsHelp!.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu7
+{
+    internal static class GaußNeedsHelp
+    {
+        public static long? RangeSum(long n) => n > 0 ? n * (n + 1) / 2 : null;
+        //https://www.codewars.com/kata/54df2067ecaa226eca000229/train/csharp
+    }
+}
