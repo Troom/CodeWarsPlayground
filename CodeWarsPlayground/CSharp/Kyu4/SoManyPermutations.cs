f74d998bf7be329ce1511f576efce8ecbe6b90c0
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-02-15 13:30:05 +0100

    Added SoManyPermutations

---
+++ CodeWarsPlayground/CSharp/Kyu4/SoManyPermutations.cs
@@ -0,0 +1,40 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu4
+{
+    //https://www.codewars.com/kata/5254ca2719453dcc0b00027d/train/csharp
+    internal static class SoManyPermutations
+    {
+        public static List<string> SinglePermutations(string s)
+        {
+            List<string> result = new List<string>();
+            Permute(s, 0, s.Length - 1, ref result);
+            return result;
+        }
+
+        private static void Permute(string str, int startingPoint, int r, ref List<string> result)
+        {
+            if (startingPoint == r) { 
+                if (!result.Contains(str))
+                 result.Add(str);  
+            }
+            else
+            {
+                for (int i = startingPoint; i <= r; i++)
+                {
+                    str = Swap(str, startingPoint, i);
+                    Permute(str, startingPoint + 1, r, ref result);
+                    str = Swap(str, startingPoint, i);
+                }
+            }
+        }
+        public static string Swap(string a, int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+    }
+}
