aceb98d97b06be2d62cdbd0c82c9866729210d20
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-02-18 23:27:12 +0100

    Added EvenOrOdd

---
+++ CodeWarsPlayground/CSharp/Kyu8/EvenOrOdd.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu8
+{
+    internal static class EvenOrOddClass
+    {
+        public static string EvenOrOdd(int number) => number % 2 == 0 ? "Even" : "Odd";
+    }
+}
