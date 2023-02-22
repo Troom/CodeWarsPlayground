09397ebea63c2c5699130a6ad8ebf12026f6f6f6
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-02-22 22:33:09 +0100

    Added PairOfGloves

---
+++ CodeWarsPlayground/CSharp/Kyu6/PairOfGloves.cs
@@ -0,0 +1,33 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu6
+{
+    internal class PairOfGloves
+    {
+        public static int NumberOfPairs(string[] gloves)
+        {
+            var tmp = new Dictionary<string, int>();
+            int counter = 0;
+
+            foreach (var item in gloves)
+            {
+                if (tmp.ContainsKey(item))
+                    tmp[item]++;
+                else
+                    tmp.Add(item, 1);
+            }
+
+
+            foreach (var item in tmp)
+            {
+                if (item.Value / 2 > 0)
+                    counter += item.Value / 2;
+            }
+            return counter;
+        }
+
+        public static int NumberOfPairsLinq(string[] gloves)
+        {
+            return gloves.GroupBy(p => p).Select(q => q.Count() / 2).Sum();
+        }
+
+    }
+}
