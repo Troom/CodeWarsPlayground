b3863585411d645144df16764001865ddd92c91f
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-02-25 21:03:10 +0100

    Added  AllStarCodeChallenge18

---
+++ CodeWarsPlayground/CSharp/Kyu8/AllStarCodeChallenge18.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu8
+{
+    internal class AllStarCodeChallenge18
+    {
+        public static int StrCount(string str, char letter)
+        {
+            int counter = 0;
+            foreach (var item in str) {
+                if (item == letter)
+                    counter++;
+            }
+            return counter;
+        }
+
+    }
+}
