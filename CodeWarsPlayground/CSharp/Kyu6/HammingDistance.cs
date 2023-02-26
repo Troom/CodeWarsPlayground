6c8a5fa6efb32a841b8e911272f82a43ff4b3ea3
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-02-26 15:33:05 +0100

    Added  HammingDistance

---
+++ CodeWarsPlayground/CSharp/Kyu6/HammingDistance.cs
@@ -0,0 +1,17 @@
+﻿
+namespace CodeWarsPlayground.CSharp.Kyu6
+{
+    internal static class HammingDistance
+    {
+        public static int Distance(string a, string b)
+        {
+            int result = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i])
+                    result++;
+            }
+            return result;
+        }
+    }
+}
