7b9d09c7d3684a6d2f0c6c4ee8b4afe9d78a25b8
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-03-04 22:20:27 +0100

    Added Countingsheep

---
+++ CodeWarsPlayground/CSharp/Kyu8/Countingsheep.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu8
+{
+    internal class Countingsheep
+    {
+        public static int CountSheeps(bool[] sheeps) => sheeps.Where(x=> x == true).Count();
+    }
+}
