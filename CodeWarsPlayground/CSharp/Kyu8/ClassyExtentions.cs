a4fed3b624100ce1630cd35866277db939736c5c
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-03-06 22:28:06 +0100

    Added ClassyExtentions

---
+++ CodeWarsPlayground/CSharp/Kyu8/ClassyExtentions.cs
@@ -0,0 +1,29 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu8
+{
+    internal class ClassyExtentions
+    {
+
+        public class Cat : Animal
+        {
+            private string _name;
+            public Cat(string name) : base(name)
+            {
+                _name = name;
+            }
+            public override string Speak()
+            {
+                return $"{_name} meows.";
+            }
+        }
+
+        public class Animal {
+
+            public Animal(string name) { 
+            }
+            public virtual string Speak() {
+                return "";
+            }
+        }
+
+    }
+}
