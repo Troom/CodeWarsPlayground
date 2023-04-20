64e62c1088be76dfa4016bdd34785b35b2daf6e4
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-04-20 15:33:32 +0200

    no message

---
+++ CodeWarsPlayground/CSharp/Kyu7/ChainMe.cs
@@ -0,0 +1,20 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu7
+{
+    internal static class ChainMe
+    {
+        public static double Chain(double input, Func<double, double>[] fs)
+        {
+            foreach (var f in fs)
+            {
+                input = f.Invoke(input);
+            }
+            return input;
+        }
+
+        public static double ChainAggregate(double input, Func<double, double>[] fs)
+        {
+            return fs.Aggregate(input, (current, t) => t(current));
+        }
+
+    }
+}
