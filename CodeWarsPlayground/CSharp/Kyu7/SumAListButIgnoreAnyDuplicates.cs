f80e4074d9c67b5910e77b39a8a11a56bef233c3
NorbertMarkiel <norbertmarkiel@gmail.com>
2023-04-21 18:31:26 +0200

    Added SumAListButIgnoreAnyDuplicates

---
+++ CodeWarsPlayground/CSharp/Kyu7/SumAListButIgnoreAnyDuplicates.cs
@@ -0,0 +1,31 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu7
+{
+    internal static class SumAListButIgnoreAnyDuplicates
+    {
+
+        public static int SumNoDuplicatesOneLiner(int[] arr) => arr.Sum(x => arr.Count(i => i == x) == 1 ? x : 0);
+
+        public static int SumNoDuplicatesDictionary(int[] arr)
+        {
+            var dic = new Dictionary<int, int>();
+
+            foreach (var i in arr)
+            {
+                if (dic.ContainsKey(i))
+                    dic[i]++;
+                else
+                    dic.Add(i, 1);
+            }
+
+            List<int> list = new List<int>();
+
+            foreach (var item in dic)
+            {
+                if (item.Value == 1)
+                    list.Add(item.Key);
+            }
+
+            return list.Sum();
+        }
+    }
+}
