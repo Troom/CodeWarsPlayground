7e4b90a5abf20b65454ff44f6af144183c2fc79e
NorbertMarkiel <norbertmarkiel@gmail.com>
2024-02-01 22:57:28 +0100

    CountTheDivisibleNumber

---
+++ CodeWarsPlayground/CSharp/Kyu6/Count the divisible numbers.cs
@@ -0,0 +1,21 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu6
+{
+    internal static class Count_the_divisible_numbers
+    {
+        public static long DivisibleCount(long x, long y, long k)
+        {
+            int result = 0;
+            for (int i = (int)x; i < y; i++)
+            {
+                if (i % k == 0)
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+
+
+        public static long DivisibleCountBetterPerformance(long x, long y, long k) => y / k - x / k + (x % k == 0 ? 1 : 0);
+    }
+}
