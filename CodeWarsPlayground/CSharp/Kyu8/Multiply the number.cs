e13c0c1e6bbe1931fc8375187c1a3f101cb2bb87
NorbertMarkiel <norbertmarkiel@gmail.com>
2024-02-14 23:10:46 +0100

    Added Multiply the number

---
+++ CodeWarsPlayground/CSharp/Kyu8/Multiply the number.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu8
+{
+    public class Multiply_the_number
+    {
+        public static int Multiply(int number) => number * ((int)Math.Pow(5, Math.Floor(Math.Log10(Math.Abs(number)) + 1)));
+    }
+}
