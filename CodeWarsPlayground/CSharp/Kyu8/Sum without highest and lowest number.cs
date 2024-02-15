f88e284f12c9d74fa9c51cf42166295529f7878c
NorbertMarkiel <norbertmarkiel@gmail.com>
2024-02-15 23:23:32 +0100

    Added Sum_without_highest_and_lowest_number

---
+++ CodeWarsPlayground/CSharp/Kyu8/Sum without highest and lowest number.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu8
+{
+    internal class Sum_without_highest_and_lowest_number
+    {
+
+        public static int Sum(int[] numbers)
+        {
+            if (numbers is null)
+                return 0;
+            if (numbers.Length < 2)
+                return 0;
+
+            return numbers.Sum() - (numbers.Min() + numbers.Max());
+        }
+
+    }
+}
