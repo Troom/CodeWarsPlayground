34f99eae697862131dc81eb6d4eb19e5cc88d72a
Norbert <norbertmarkiel@gmail.com>
2024-05-08 18:07:28 +0200

    Added GrillIt

---
+++ CodeWarsPlayground/CSharp/Kyu6/Grill it!.cs
@@ -0,0 +1,30 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu6
+{
+    internal class Grill_it_
+    {
+
+        public static string Grille(string message, int code)
+        {
+            string binaryCode = Convert.ToString(code, 2).PadLeft(message.Length, '0');
+            string decryptedMessage = "";
+            if (message.Length < binaryCode.Length)
+                binaryCode = binaryCode.Substring(binaryCode.Length - message.Length);
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (binaryCode[i] == '1')
+                {
+                    decryptedMessage += message[i];
+                }
+            }
+            return decryptedMessage;
+        }
+        public static string Grille1(string message, int code)
+        {
+            string mapper = (mapper = new string('0', message.Length) + Convert.ToString(code, 2)).Substring(mapper.Length - message.Length);
+            return string.Concat(message.Where((e, i) => mapper[i] == '1'));
+        }
+
+        public static string GrilleLinq(string m, int code) =>
+            String.Concat(m.Where((c, i) => ((long)1 << m.Length - i - 1 & code) != 0));
+    }
+}
