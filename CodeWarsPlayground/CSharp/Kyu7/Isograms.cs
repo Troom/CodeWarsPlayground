6dce576ee226a6753cf67256ddc3e5ae098e570b
Norbert <norbertmarkiel@gmail.com>
2024-05-11 20:17:46 +0200

    Add Isograms

---
+++ CodeWarsPlayground/CSharp/Kyu7/Isograms.cs
@@ -0,0 +1,18 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu7
+{
+    public class Isograms
+    {
+        public static bool IsIsogram(string str)
+        {
+            str = str.ToLower();
+            HashSet<char> seen = new HashSet<char>();
+            foreach (char ch in str)
+            {
+                if (seen.Contains(ch))
+                    return false;
+                seen.Add(ch);
+            }
+            return true;
+        }
+    }
+}
