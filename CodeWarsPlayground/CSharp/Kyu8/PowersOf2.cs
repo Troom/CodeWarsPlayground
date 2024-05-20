b43f6d515147523b9356d7582a7b60d6e375d391
Norbert <norbertmarkiel@gmail.com>
2024-05-20 17:30:44 +0200

    Added PowersOf2

---
+++ CodeWarsPlayground/CSharp/Kyu8/PowersOf2.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace CodeWarsPlayground.CSharp.Kyu8
+{
+    public static class PowersOf2
+    {
+        public static BigInteger[] PowersOfTwo(int n)
+        {
+            List<BigInteger> result = new List<BigInteger>();
+            for (int i = 0; i <= n; i++)
+            {
+                result.Add((BigInteger)Math.Pow(2, i));
+            }
+            return result.ToArray();
+        }
+    }
+}
