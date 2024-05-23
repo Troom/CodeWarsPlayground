118c1ca826f4a88d75b0fffd1f7bb7e22a357cec
Norbert <norbertmarkiel@gmail.com>
2024-05-23 18:15:19 +0200

    Add CategorizeNewMember

---
+++ CodeWarsPlayground/CSharp/Kyu7/CategorizeNewMember.cs
@@ -0,0 +1,26 @@
+﻿namespace CodeWarsPlayground.CSharp.Kyu7
+{
+    public static class CategorizeNewMember
+    {
+        public static IEnumerable<string> OpenOrSenior(int[][] data)
+        {
+            var result = new List<string>();
+            foreach (var item in data)
+            {
+                if (item[0] >= 55)
+                {
+                    if (item[1] > 7)
+                    {
+                        result.Add("Senior");
+                        continue;
+                    }
+                }
+                result.Add("Open");
+            }
+            return result;
+        }
+
+        public static IEnumerable<string> OpenOrSeniorSelect(int[][] data) =>
+                 data.Select(member => member[0] >= 55 && member[1] > 7 ? "Senior" : "Open").ToList();
+    }   
+}
